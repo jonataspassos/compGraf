b991394ba8405f520b447fe330244aca7cd27ba2
jonataspassos <jonatascastropassos@gmail.com>
2019-10-25 00:18:59 -0300

    Testando Bresehan

--- 01_formasPrimitivas/algoritmosRetas.js
+++ 01_formasPrimitivas/algoritmosRetas.js
@@ -1,10 +1,10 @@
 function Algoritmo_DDA(x1, y1, x2, y2) {
     var troca = false;
-    if(x1 == x2){
+    if (x1 == x2) {
         var temp = x1;
         x1 = y1;
         y1 = temp;
-        
+
         temp = x2;
         x2 = y2;
         y2 = temp;
@@ -13,11 +13,11 @@ function Algoritmo_DDA(x1, y1, x2, y2) {
     }
 
 
-    if(x1>x2){
+    if (x1 > x2) {
         var temp = x1;
         x1 = x2;
         x2 = temp;
-        
+
         temp = y1;
         y1 = y2;
         y2 = temp;
@@ -36,7 +36,7 @@ function Algoritmo_DDA(x1, y1, x2, y2) {
 
     while (X <= x2) {
         //console.log('x=', X, 'y=', Y)
-        coordenadas = { t: it, c: [{ x: Math.round(troca?Y:X), y: Math.round(troca?X:Y) }] }
+        coordenadas = { t: it, c: [{ x: Math.round(troca ? Y : X), y: Math.round(troca ? X : Y) }] }
         draw_pixel(coordenadas);
         X = X + Xinc
         Y = Y + Yinc
@@ -45,60 +45,40 @@ function Algoritmo_DDA(x1, y1, x2, y2) {
 }
 
 function Algoritmo_Bresenham(x1, y1, x2, y2) {
-    
-    var troca = false;
 
-    if(x1>x2){
-        var temp = x1;
-        x1 = x2;
-        x2 = temp;
-        
-        temp = y1;
-        y1 = y2;
-        y2 = temp;
+    let troca = false
+    if (Math.abs(x1 - x2) < Math.abs(y1 - y2)) {
+        let aux = x1
+        x1 = y1
+        y1 = aux
+        aux = x2
+        x2 = y2
+        y2 = aux
+        troca = true
     }
 
-    var dx = x2 - x1
-    var dy = y2 - y1
-    let p = 2 * dy - dx
-    let x = x1, y = 0, it = 0
-    let coordenadas = { t: it, c: [{ x: troca?y:x, y: troca?x:y }] }
-    draw_pixel(coordenadas);
-    while (x < x2) {
-        it += 1
-        if (p < 0) {
-            p = p + 2 * dy
-        }
-        else {
-            p = p + 2 * dy - 2 * dx
-            y += 1
-        }
-        //console.log('x = ', x, 'y = ', y)
-        x += 1
-        coordenadas = { t: it, c: [{ x: troca?y:x, y: troca?x:y }] }
-        draw_pixel(coordenadas);
+    let dx, dy, p, x
+    if (x1 > x2) {
+        let aux = x1
+        x1 = x2
+        x2 = aux
+        aux = y1
+        y1 = y2
+        y2 = aux
     }
-
-}
-
-
-/**
- * function Algoritmo_Bresenham(x1, y1, x2, y2) {
-    let offset, dx, dy, p, x
     if (y1 > y2) {
         dy = y1 - y2
     }
     else {
         dy = y2 - y1
     }
-    offset = y2
     dx = x2 - x1
     p = 2 * dy - dx
     x = x1
     y = y1
     it = 0
 
-    let coordenadas = { t: it, c: [{ x: x, y: Math.floor(y) }] }
+    let coordenadas = { t: it, c: [{ x: Math.round(troca ? y : x), y: Math.round(troca ? x : y) }] }
     draw_pixel(coordenadas);
     while (x < x2) {
         it += 1
@@ -115,9 +95,8 @@ function Algoritmo_Bresenham(x1, y1, x2, y2) {
         }
         x += 1
         console.log('x = ', x, 'y = ', y)
-        coordenadas = { t: it, c: [{ x: x, y: Math.floor(y) }] }
+        coordenadas = { t: it, c: [{ x: Math.round(troca ? y : x), y: Math.round(troca ? x : y) }] }
         draw_pixel(coordenadas);
     }
 
 }
- */
